541977a4d360a68cefd744c116c66c9e829ad209
katarzynatrzaska <42570868+katarzynatrzaska@users.noreply.github.com>
2018-09-25 12:59:31 +0200

    Annotations Added
    Added:
    @beforeTest
    @AfterTest
    protected static WebDriver driver;
    Changed public void to: public void HeaderUrlRedirect()

--- HeaderBanner.java
+++ HeaderBanner.java
@@ -1,21 +1,36 @@
-package automationpractice.com;
+package automationpractise.com;
+
+import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 @Test
 
 public class HeaderBanner {
+	protected static WebDriver driver;
 	
-	
-	public void testng () {
-
+	@BeforeTest()
+	public void beforeTest()
+	{
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	}
+	@AfterTest()
+	public void afterTest() 
+	{
+		driver.quit();
+	}
 
+	@Test(priority = 0)
+	public void HeaderUrlRedirect() {
 		/* Go to main page */
+		driver = new ChromeDriver();
+
 		driver.get("http://automationpractice.com/index.php");
 
 		/* header banner - verify redirect URL */
@@ -23,8 +38,8 @@ public class HeaderBanner {
 		WebElement banner = driver.findElement(
 				By.cssSelector("[src=\"http://automationpractice.com/modules/blockbanner/img/sale70.png\"]"));
 		banner.click();
-	
-		assertEquals(driver.getCurrentUrl(),bannerURL);
+
+		assertEquals(driver.getCurrentUrl(), bannerURL);
 	}
 
 }
