13fae9ec405ecf8cad0f39a468f8705f66b0102c
katarzynatrzaska <42570868+katarzynatrzaska@users.noreply.github.com>
2018-09-25 15:04:55 +0200

    Added annotations, changed assert method

--- LogoRedirect.java
+++ LogoRedirect.java
@@ -1,25 +1,38 @@
-package automationpractice.com;
+package automationpractise.com;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-@Test
-public class LogoRedirect {
 
-	public void testng () {
+public class LogoRedirect {
+	protected static WebDriver driver;
+
+	@BeforeTest()
+	public void beforeTest() {
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	}
+
+	@AfterTest()
+	public void afterTest() {
+		driver.quit();
+	}
+
+	@Test(priority = 0)
+	public void logoRedirectURL() {
+		driver = new ChromeDriver();
 
 		/* Go to main page */
 		driver.get("http://automationpractice.com/index.php");
 
 		/* Your Logo img - verify redirect URL */
-		String yourLogoURL = "http://automationpractice.com/index.php";
 		WebElement yourLogo = driver.findElement(By.cssSelector("[src=\"http://automationpractice.com/img/logo.jpg"));
 		yourLogo.click();
-		assertEquals(driver.getCurrentUrl(),yourLogoURL);
-
+		driver.getCurrentUrl();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://automationpractice.com/index.php");
 	}
 }
