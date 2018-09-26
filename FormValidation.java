f9f730f2c5e6d6485ca3437f19d131e812cdeec8
katarzynatrzaska <42570868+katarzynatrzaska@users.noreply.github.com>
2018-09-26 14:34:33 +0200

    Updates
    @AfterTest() added
    deleted unnecessary WebElement

--- FormValidation.java
+++ FormValidation.java
@@ -1,10 +1,11 @@
-package automationpractise.com;
+package automationpractice.com;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,30 +17,26 @@ public class FormValidation {
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
 	}
 
+	@AfterTest()
+	public void afterTest() {
+		driver.quit();
+	}
+
 	@Test(priority = 0)
 	public void blankFormTest() {
 		driver = new ChromeDriver();
 		driver.get("http://automationpractice.com/index.php?controller=contact");
-
-		WebElement SendButton = driver.findElement(By.id("submitMessage"));
-		SendButton.click();
+		driver.findElement(By.id("submitMessage")).click();
 		WebElement ErrorField = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/ol/li"));
-		{
-			Assert.assertEquals(ErrorField.getText(), "Invalid email address.");
-
-		}
+		Assert.assertEquals(ErrorField.getText(), "Invalid email address.");
 	}
 
 	@Test(priority = 1)
 	public void correctEmailonly() {
-		WebElement Email = driver.findElement(By.id("email"));
-		Email.sendKeys("kasiatrzaska@o2.pl");
-		WebElement SendButton = driver.findElement(By.id("submitMessage"));
-		SendButton.click();
+		driver.findElement(By.id("email")).sendKeys("kasiatrzaska@o2.pl");
+		driver.findElement(By.id("submitMessage")).click();
 		WebElement ErrorField = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/ol/li"));
-		{
-			Assert.assertEquals(ErrorField.getText(), "The message cannot be blank.");
-		}
-
+		Assert.assertEquals(ErrorField.getText(), "The message cannot be blank.");
 	}
+
 }
